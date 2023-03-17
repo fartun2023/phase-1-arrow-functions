f843c544e7f6e3bb809beaa94c3f11203edb77a2
fartun <fartunwario34@gmail.com>
2023-03-17 22:17:28 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(para1, para2) {
+    return(2000/100)
+}
+//2
+const square = (para1, para2)=>para1*para1
+let para1 = 2;
+square(2*2);
+
+//3
+const add = (para1, para2) =>para1+para2
